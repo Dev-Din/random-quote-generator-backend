5520fdbfae8985dbcb2701da48c47b013c8c0260
nurdinhajo <nurudiin222@gmail.com>
2023-08-04 18:24:04 +0300

    API ~> Random quote

--- quote_generator_backend/app.rb
+++ quote_generator_backend/app.rb
@@ -1,2 +1,16 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+
+# Set up database connection 
+set :database, { adapter: 'sqlite3', database: 'db/quotes.db' }
+
+class Quote < ActiveRecord::Base
+end
+
+# API to get random quote
+get '/quotes/random' do
+  content_type :json
+  quote = Quote.order('RANDOM()').first
+  quote.to_json
+end
+
